2f58452f089f631a4191eeffdb575dc656dfe501
Dolitha Chowdary <155249751+dolithachowdary@users.noreply.github.com>
2025-07-20 10:32:04 +0530

    Update day2.py

--- day2.py
+++ day2.py
@@ -1,85 +1,156 @@
-
-# missing positive
 def miss(a):
-    for i in range(1,max(a)):
+    """
+    Finds and prints the smallest missing positive integer in the list.
+    
+    Parameters:
+    a (list): A list of positive integers.
+    
+    Example:
+    >>> miss([1, 3, 4])
+    2
+    """
+    for i in range(1, max(a)):
         if i not in a:
             print(i)
-miss([1,3,4])
 
-#move zeros to last
 def zero(a):
-    n=[]
-    z=[]
+    """
+    Moves all zeros in the list to the end, maintaining the order of non-zero elements.
+    
+    Parameters:
+    a (list): A list of integers.
+    
+    Returns:
+    list: Modified list with zeros at the end.
+    
+    Example:
+    >>> zero([1, 2, 0, 3, 4, 0])
+    [1, 2, 3, 4, 0, 0]
+    """
+    n = []
+    z = []
     for i in a:
-        if i==0:
+        if i == 0:
             z.append(i)
         else:
             n.append(i)
-    return n+z
-print(zero([1,2,0,3,4,0,5,6,0]))
- 
-#move even to last 
+    return n + z
+
 def even(a):
-    n=[]
-    e=[]
+    """
+    Moves all even numbers in the list to the end, maintaining the order of odd elements.
+    
+    Parameters:
+    a (list): A list of integers.
+    
+    Returns:
+    list: Modified list with even numbers at the end.
+    
+    Example:
+    >>> even([1, 2, 3, 4])
+    [1, 3, 2, 4]
+    """
+    n = []
+    e = []
     for i in a:
-        if i%2==0:
+        if i % 2 == 0:
             e.append(i)
         else:
             n.append(i)
-    return n+e
-print(even([1,2,3,4,5,6]))
+    return n + e
 
 def fun(a):
-    n=len(a)
-    for i in range(0,n//2):
-        if a[i]==0:
-            a[i],a[n-i]=a[n-i],a[i]
+    """
+    Attempts to swap zeros from the first half of the list with elements from the end.
+    
+    NOTE: This has an indexing bug. It should use a[n-i-1] instead of a[n-i].
+    
+    Parameters:
+    a (list): A list of integers.
+    
+    Returns:
+    list: Modified list after attempted swap.
+    """
+    n = len(a)
+    for i in range(0, n // 2):
+        if a[i] == 0:
+            a[i], a[n - i] = a[n - i], a[i]  # BUG: should be a[n-i-1]
     return a
-print(fun([1,2,0,4,3,5,0]))
 
 def sol(a):
-    for i in a:
-        if i==0:
-            a.remove(i)
-            a.append(i)
-    return a 
+    """
+    Moves all zeros to the end by removing and appending them during iteration.
     
-def sol2(a):
+    WARNING: Modifying the list while iterating may cause unexpected behavior.
+    
+    Parameters:
+    a (list): A list of integers.
+    
+    Returns:
+    list: Modified list with zeros at the end.
+    """
     for i in a:
-        if i%2==0:
+        if i == 0:
             a.remove(i)
             a.append(i)
-    return a 
-print(sol([1,3,0,0,4,0,2]))
-print(sol2([1,3,6,4,5,8]))  
+    return a
+
+def sol2(a):
+    """
+    Moves all even numbers to the end by removing and appending during iteration.
+    
+    WARNING: Modifying the list while iterating may cause skipping of elements.
+    
+    Parameters:
+    a (list): A list of integers.
+    
+    Returns:
+    list: Modified list with even numbers at the end.
+    """
+    for i in a:
+        if i % 2 == 0:
+            a.remove(i)
+            a.append(i)
+    return a
 
 def two(a):
-    k=0
-    i=0
+    """
+    Moves all odd numbers to the front while maintaining their original order.
+    
+    Parameters:
+    a (list): A list of integers.
+    
+    Returns:
+    list: Modified list with odd numbers first.
+    
+    Example:
+    >>> two([1, 3, 6, 4, 5, 8])
+    [1, 3, 5, 6, 4, 8]
+    """
+    k = 0
+    i = 0
     for i in range(len(a)):
-        if a[i]%2!=0:
-            temp=a.pop(i)
-            a.insert(k,temp)
-            k+=1
-    return a 
-print(two([1,3,6,4,5,8]))  
-
+        if a[i] % 2 != 0:
+            temp = a.pop(i)
+            a.insert(k, temp)
+            k += 1
+    return a
 
 def bit(a):
-    count=0
-    while a>0:
-        r=a%2
-        if r!=0:
-            count+=1
-        a=a//2
+    """
+    Counts the number of set bits (1s) in the binary representation of the given number.
+    
+    Parameters:
+    a (int): A positive integer.
+    
+    Example:
+    >>> bit(1245)
+    7
+    """
+    count = 0
+    while a > 0:
+        r = a % 2
+        if r != 0:
+            count += 1
+        a = a // 2
     print(count)
-bit(1245)
-
-
-
-
-
-
-
-
